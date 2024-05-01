4e6b54a6979d75c621c70cc89cfb2c506a0c912d
nagur2 <hayani0000@gmail.com>
2024-05-01 15:05:06 +0900

    주소기능변경
    함수 리펙토링, 그래프 절대값 추가

--- IPO Analysis Program/add.py
+++ IPO Analysis Program/add.py
@@ -16,16 +16,18 @@ class add:
     failDf = df[df['성공여부'].str.contains('심사철회|심사미승인')]
     okDf = df[df['성공여부'].str.contains('심사승인')]
 
-    downCount = 0
-
+    downCount = 5   # 각 버튼이 클릭되는 것을 알리는 플래그 변수
+    
+    # 버튼 구현
     def btn(self, dialog):
         dialog.addAllBtn.clicked.connect(lambda: self.graphAll(dialog))
         dialog.addReadyBtn.clicked.connect(lambda: self.graphReady(dialog))
         dialog.addFailBtn.clicked.connect(lambda: self.graphFail(dialog))
         dialog.addOkBtn.clicked.connect(lambda: self.graphOk(dialog))
         dialog.addDown.clicked.connect(self.down)
-        dialog.addTable.setEditTriggers(QAbstractItemView.NoEditTriggers)
-
+        dialog.addTable.setEditTriggers(QAbstractItemView.NoEditTriggers) # QTable위젯 수정물가 코드
+    
+    # 각 함수에서 사용할 데이터 정제
     def countDf(self, df):
         df2 = df['주소'].str[:2]
         seoulCount = len(df[df2.str.contains('서울')])
@@ -47,37 +49,16 @@ class add:
         })
         return result_df
 
-
+    # 전체 버튼 클릭시 구현될 함수
     def graphAll(self, dialog):
         self.downCount = 0
-        seoul_map = folium.Map(
-            location=[35.55, 126.38],
-            zoom_start=7,
-            tiles='CartoDB positron'
-        )
-
+        seoul_map = self.mapPrint()
         df = self.countDf(self.df)
 
         count = 0
         for name, lat, lng in zip(df.지역, df.위도, df.경도):
-            print(len(name))
             ban = int(df['count'][count]),  # 원의 반지름
-
-            if ban[0] >= 800:
-                result = 100
-            elif ban[0] >= 400:
-                result = 80
-            elif ban[0] >= 170:
-                result = 50
-            elif ban[0] >= 160:
-                result = 48
-            elif ban[0] >= 30:
-                result = 15
-            elif ban[0] >= 10:
-                result = 5
-            else:
-                result = ban[0]
-
+            result = self.onePrint(ban[0])
 
             folium.CircleMarker(
                 [lat, lng],
@@ -87,7 +68,6 @@ class add:
                 fill_color='navy',  # 원 안의 색상
                 fill_opacity=0.7,  # 원의 투명도
             ).add_to(seoul_map)
-            # print(df['count'][count])
             count += 1
 
         count = 0
@@ -108,36 +88,22 @@ class add:
         html_file_path = os.path.join(current_dir, 'map.html')
         dialog.mapGraph.setUrl(QUrl.fromLocalFile(html_file_path))
 
-        tableHeader = ['지역', '법인 수']
-        # print(len(df))
-        dialog.addTable.setRowCount(len(df))
-        dialog.addTable.setColumnCount(2)
-        dialog.addTable.setHorizontalHeaderLabels(tableHeader)
+        self.summary(dialog, df)
 
-        for i in range(len(df)):
-            for j in range(2):
-                item = QTableWidgetItem(str(df.iloc[i, j]))  # 문자열로 변환하여 QTableWidgetItem 생성
-                dialog.addTable.setItem(i, j, item)
-        dialog.addTable.resizeColumnsToContents()
-
-
+    # 심사대기 버튼 클릭시 구현될 함수
     def graphReady(self, dialog):
         self.downCount = 1
-        seoul_map = folium.Map(
-            location=[35.55, 126.38],
-            zoom_start=7,
-            tiles='CartoDB positron'
-        )
+        seoul_map = self.mapPrint()
         df = self.countDf(self.readyDf)
 
         count = 0
         for name, lat, lng in zip(df.지역, df.위도, df.경도):
             ban = int(df['count'][count]),  # 원의 반지름
-            # print(ban[0])
+            result = self.onePrint(ban[0])
 
             folium.CircleMarker(
                 [lat, lng],
-                radius=ban[0]+10,  # 원의 반지름
+                radius=result,  # 원의 반지름
                 color='green',  # 원의 둘레 색상
                 fill=True,  # 원 안을 채우기
                 fill_color='green',  # 원 안의 색상
@@ -162,35 +128,22 @@ class add:
         html_file_path = os.path.join(current_dir, 'mapReady.html')
         dialog.mapGraph.setUrl(QUrl.fromLocalFile(html_file_path))
 
-        tableHeader = ['지역', '법인 수']
-        # print(len(df))
-
-        dialog.addTable.setRowCount(len(df))
-        dialog.addTable.setColumnCount(2)
-        dialog.addTable.setHorizontalHeaderLabels(tableHeader)
-        for i in range(len(df)):
-            for j in range(2):
-                item = QTableWidgetItem(str(df.iloc[i, j]))  # 문자열로 변환하여 QTableWidgetItem 생성
-                dialog.addTable.setItem(i, j, item)
-        dialog.addTable.resizeColumnsToContents()
-
+        self.summary(dialog, df)
+    
+    # 심사탈락 버튼 클릭시 구현될 함수
     def graphFail(self, dialog):
         self.downCount = 2
-        seoul_map = folium.Map(
-            location=[35.55, 126.38],
-            zoom_start=7,
-            tiles='CartoDB positron'
-        )
+        seoul_map = self.mapPrint()
         df = self.countDf(self.failDf)
 
         count = 0
         for name, lat, lng in zip(df.지역, df.위도, df.경도):
             ban = int(df['count'][count]),  # 원의 반지름
-            # print(ban[0])
+            result = self.onePrint(ban[0])
 
             folium.CircleMarker(
                 [lat, lng],
-                radius=ban[0]+10,  # 원의 반지름
+                radius=result,  # 원의 반지름
                 color='red',  # 원의 둘레 색상
                 fill=True,  # 원 안을 채우기
                 fill_color='red',  # 원 안의 색상
@@ -215,34 +168,22 @@ class add:
         html_file_path = os.path.join(current_dir, 'mapFail.html')
         dialog.mapGraph.setUrl(QUrl.fromLocalFile(html_file_path))
 
-        tableHeader = ['지역', '법인 수']
-        # print(len(df))
-        dialog.addTable.setRowCount(len(df))
-        dialog.addTable.setColumnCount(2)
-        dialog.addTable.setHorizontalHeaderLabels(tableHeader)
-        for i in range(len(df)):
-            for j in range(2):
-                item = QTableWidgetItem(str(df.iloc[i, j]))  # 문자열로 변환하여 QTableWidgetItem 생성
-                dialog.addTable.setItem(i, j, item)
-        dialog.addTable.resizeColumnsToContents()
-
+        self.summary(dialog, df)
+    
+    # 심사통과 버튼 클릭시 구현될 함수
     def graphOk(self, dialog):
         self.downCount = 3
-        seoul_map = folium.Map(
-            location=[35.55, 126.38],
-            zoom_start=7,
-            tiles='CartoDB positron'
-        )
+        seoul_map = self.mapPrint()
         df = self.countDf(self.okDf)
 
         count = 0
         for name, lat, lng in zip(df.지역, df.위도, df.경도):
             ban = int(df['count'][count]),  # 원의 반지름
-            # print(ban[0])
+            result = self.onePrint(ban[0])
 
             folium.CircleMarker(
                 [lat, lng],
-                radius=ban[0]+10,  # 원의 반지름
+                radius=result,  # 원의 반지름
                 color='blue',  # 원의 둘레 색상
                 fill=True,  # 원 안을 채우기
                 fill_color='blue',  # 원 안의 색상
@@ -267,16 +208,7 @@ class add:
         html_file_path = os.path.join(current_dir, 'mapOk.html')
         dialog.mapGraph.setUrl(QUrl.fromLocalFile(html_file_path))
 
-        tableHeader = ['지역', '법인 수']
-        # print(len(df))
-        dialog.addTable.setRowCount(len(df))
-        dialog.addTable.setColumnCount(2)
-        dialog.addTable.setHorizontalHeaderLabels(tableHeader)
-        for i in range(len(df)):
-            for j in range(2):
-                item = QTableWidgetItem(str(df.iloc[i, j]))  # 문자열로 변환하여 QTableWidgetItem 생성
-                dialog.addTable.setItem(i, j, item)
-        dialog.addTable.resizeColumnsToContents()
+        self.summary(dialog, df)
 
     def down(self):
         try:
@@ -299,11 +231,98 @@ class add:
                 df = self.countDf(self.okDf)
                 selected_columns = df.iloc[:, :2]  # 0번과 1번 열 선택
                 selected_columns.to_excel("IPO 승인 완료 기업 수 데이터_주소별.xlsx", index=True)
+
         except Exception as e:
             print(e)
             print(traceback.print_exc())
 
-        
+    # 지도에 원그래프 절대값 설정 
+    def onePrint(self, ban):
+        try:
+            if self.downCount == 0:
+                if ban >= 800:
+                    result = 120
+                elif ban >= 400:
+                    result = 100
+                elif ban >= 170:
+                    result = 80
+                elif ban >= 160:
+                    result = 75
+                elif ban >= 30:
+                    result = 20
+                elif ban >= 10:
+                    result = 10
+                else:
+                    result = 3
+
+                return result
+
+            elif self.downCount == 1:
+                if ban >= 20:
+                    result = 20
+                elif ban >= 10:
+                    result = 10
+                elif ban == 0:
+                    result = 1
+                else:
+                    result = ban
+
+                return result
+
+            elif self.downCount == 2:
+                if ban >= 100:
+                    result = 60
+                elif ban >= 60:
+                    result = 35
+                elif ban >= 30:
+                    result = 20
+                elif ban >= 25:
+                    result = 15
+                else:
+                    result = 3
+
+                return result
+
+            elif self.downCount == 3:
+                if ban >= 600:
+                    result = 110
+                elif ban >= 300:
+                    result = 90
+                elif ban >= 100:
+                    result = 60
+                elif ban >= 20:
+                    result = 15
+                elif ban >= 10:
+                    result = 10
+                else:
+                    result = 3
+
+                return result
+        except Exception as e:
+            print(e)
+            print(traceback.print_exc())
+    
+    # 서머리 출력 함수
+    def summary(self, dialog,  df):
+        tableHeader = ['지역', '법인 수']
+
+        dialog.addTable.setRowCount(len(df))
+        dialog.addTable.setColumnCount(2)
+        dialog.addTable.setHorizontalHeaderLabels(tableHeader)
+        for i in range(len(df)):
+            for j in range(2):
+                item = QTableWidgetItem(str(df.iloc[i, j]))  # 문자열로 변환하여 QTableWidgetItem 생성
+                dialog.addTable.setItem(i, j, item)
+        dialog.addTable.resizeColumnsToContents()   # 컬럼 사이즈를 글씨크기에 맞게 조정
+
+    # 지도 생성 메서드
+    def mapPrint(self):
+        seoul_map = folium.Map(
+            location=[35.55, 126.38],
+            zoom_start=7,
+            tiles='CartoDB positron'
+        )
+        return seoul_map
 
 
 
