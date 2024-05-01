92261868175472a48542162222b2a8daf77a6c28
kimjisooooo <9729935@naver.com>
2024-05-01 11:50:50 +0900

    주선인 pie차트 bar차트 업데이트
    reverse와 서머리 구현해야 함

--- IPO Analysis Program/patest.py
+++ IPO Analysis Program/patest.py
@@ -3,6 +3,7 @@ import traceback
 
 import pandas as pd
 import plotly.graph_objects as go
+import self
 from PyQt5.QtCore import QUrl
 
 # Test 클래스 정의
@@ -13,9 +14,8 @@ class Gamsain:
             return processed_values.explode()
         else:
             return column_series
-
+#====================전체 조회=============================================================
     def create_gamsain_chart(self, dialog, column_series, threshold=0.01):
-        # print(column_series)
         try:
             processed_column = self.process_column(column_series)
             value_counts = processed_column.value_counts()
@@ -32,3 +32,69 @@ class Gamsain:
             print(e)
             print(traceback.format_exc())
 
+    # def reverse_sunggong_chart(self):
+# ====================성공률 top10=============================================================
+    def calculate_success_rates(self, df):
+        # '심사승인'인 데이터만 필터링
+        success_df = df[df['성공여부'] == '심사승인']
+
+        # 쉼표가 없는 데이터만 필터링
+        success_df = success_df[~success_df['주선인'].str.contains(',')]
+
+        # 주선인별 데이터 수 계산
+        counts = df['주선인'].value_counts()
+
+        # 데이터 수가 50개 이상인 주선인만 선택
+        qualified_counts = counts[counts >= 50]
+
+        # 성공률 계산
+        success_counts = success_df['주선인'].value_counts()
+        success_rates = (success_counts / qualified_counts) * 100
+
+        # 성공률 상위 10개 추출
+        top_10_success_rates = success_rates.nlargest(10)
+
+        return top_10_success_rates
+
+
+    def create_top_success_rates_bar_chart(self, dialog, df):
+        # 함수 호출
+        top_10_success_rates = self.calculate_success_rates(df)
+
+        # 그래프 생성
+        fig = go.Figure(data=[go.Bar(x=top_10_success_rates.index, y=top_10_success_rates.values,
+                                     text=top_10_success_rates.round(2).astype(str) + '%',
+                                     textposition='auto')])  # 'auto'는 최적 위치에 자동 배치
+        fig.update_layout(title='성공률 주선인 Top10(상장승인 50건 이상)')
+
+        # HTML 파일로 저장
+        fig.write_html("성공률 top10.html")
+        html_path = os.path.abspath("성공률 top10.html")
+        dialog.gamsain_webEngineView.setUrl(QUrl.fromLocalFile(html_path))
+#================승인횟수 Top10==========================================================================
+
+    def create_gamsain_barchart(self, dialog, column_series, threshold=0.01):
+        try:
+            processed_column = self.process_column(column_series)  # 주선인 열 처리
+            value_counts = processed_column.value_counts()
+            total_count = value_counts.sum()
+            filtered_counts = value_counts[value_counts / total_count >= threshold]
+            etc_count = value_counts[value_counts / total_count < threshold].sum()
+            filtered_counts['etc'] = etc_count
+
+            # top 10 추출
+            top_10_counts = filtered_counts.nlargest(10)
+
+            # 그래프 생성
+            fig = go.Figure(data=[go.Bar(x=top_10_counts.index, y=top_10_counts.values,
+                                         text=top_10_counts.round(2).astype(str) + '건',
+                                         textposition='auto')])
+            fig.update_layout(title='상장 주선인 Top10(상장승인 50건 이상)')
+
+            # HTML 파일로 저장
+            fig.write_html("상장 주선인 Top10.html")
+            html_path = os.path.abspath("상장 주선인 Top10.html")
+            dialog.gamsain_webEngineView.setUrl(QUrl.fromLocalFile(html_path))
+        except Exception as e:
+            print(e)
+            print(traceback.format_exc())
--- IPO Analysis Program/view.py
+++ IPO Analysis Program/view.py
@@ -6,6 +6,8 @@ from add import add
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication
 import pandas as pd
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView
 
 # 경로 지정?
 root = os.path.dirname(os.path.abspath(__file__))
@@ -18,21 +20,25 @@ class MainDialog(QMainWindow, MainUI):
             super().__init__()
             self.setupUi(self)  # This must come before any usage of UI elements
             self.setWindowTitle('Main')
+            
+            # -- 성준
             self.sector = sectors()
             self.secAllBtn.clicked.connect(lambda: self.sector.secAllBtn_clicked(self))
             self.secAllBtn.clicked.connect(lambda: self.sector.secReadyBtn_clicked(self))
             self.secAllBtn.clicked.connect(lambda: self.sector.secFailBtn_clicked(self))
             self.secAllBtn.clicked.connect(lambda: self.sector.secOkBtn_clicked(self))
-
+            
+            # -- 나겸
             self.add = add()
             self.add.btn(self)
-
-            self.gamasain_instance = Gamsain()
+            
+            
+            # -- 지수
+            self.test_instance = Gamsain()
             df = pd.read_csv("IPO현황_최종.csv", encoding="cp949")
-            self.gamasain_instance.create_gamsain_chart(self, df['주선인'])
-
-
-
+            self.paAllBtn.clicked.connect(lambda: self.test_instance.create_gamsain_chart(self, df['주선인']))
+            self.okRateBtn.clicked.connect(lambda: self.test_instance.create_top_success_rates_bar_chart(self, df))
+            self.okBtn.clicked.connect(lambda: self.test_instance.create_gamsain_barchart(self, df['주선인']))
     except Exception as e:
         print(e)
         print(traceback.format_exc())
